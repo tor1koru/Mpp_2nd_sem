9cbe2b1f17e48aed5816f9fe7fc1b5379d46d5ff
EgorVolovetski <egorokav48@gmail.com>
2024-04-21 01:11:42 +0300

    products page updated

--- Online_Store/online_store/config/routes.rb
+++ Online_Store/online_store/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'customers/index'
   get 'products/index'
   root 'home#index'
   get 'cart', to: 'home#cart'
   get 'about', to: 'home#about'
   get 'contact', to: 'home#contact'
   get 'index', to: 'home#index'
-
+  get '/home/index', to: 'home#index'
+  get 'customers/index', to: 'customers#index'
   resources :products
+  resources :customers
 end
